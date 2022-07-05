242253e8f386e561ec015af7133a4462fc86cba0
hyunjinP <wldwlddl0531@gmail.com>
2022-07-05 15:31:58 +0900

    try

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/image', upload.single('image'), (req, res)=>{
     const file = req.file;
     console.log(file);
     res.send({
-        imageUrl: "https://server-teacher.herokuapp.com/"+file.destination+file.filename
+        imageUrl: "https://server-teacher.herokuapp.com"+file.destination+file.filename
     })
 })
 
